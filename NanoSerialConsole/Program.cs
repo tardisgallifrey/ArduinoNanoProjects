f0c65cc5bb3f70e46dac5820659b9499e5c4f10f
Dave <bigmanrunning@gmail.com>
2021-01-02 12:47:14 -0600

    first commit

---
+++ NanoSerialConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NanoSerialConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ NanoSerialLib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace NanoSerialLib
+{
+    public class Class1
+    {
+    }
+}
